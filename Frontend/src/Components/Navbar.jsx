adb02fbed61da3c9ffc2a84b134185532df64774
dushyanth-9901 <dushyanth-9901@gmail.com>
2026-05-18 20:04:27 +0530

    changed buttons postion

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -4,6 +4,8 @@ import { useNavigate } from "react-router-dom";
 function Navbar({ cart }) {
 
   const navigate = useNavigate();
+  const isLoggedIn =
+  localStorage.getItem("isLoggedIn");
 
   // 🔓 Logout
   const logout = () => {
@@ -20,21 +22,54 @@ function Navbar({ cart }) {
       {/* 🔹 Right Side */}
       <div style={styles.right}>
 
-        {/* 🔹 Cart Button */}
-        <button
-          style={styles.cartBtn}
-          onClick={() => navigate("/cart")}
-        >
-          Cart ({cart.length})
-        </button>
+           {/* 🔹 Login/Logout Button */}
+                  {
+            isLoggedIn ? (
 
-        {/* 🔹 Logout */}
-        <button
-          style={styles.logoutBtn}
-          onClick={logout}
-        >
-          Logout
-        </button>
+              <button
+                style={styles.logoutBtn}
+                onClick={() => {
+
+                  localStorage.removeItem(
+                    "isLoggedIn"
+                  );
+
+                  window.location.reload();
+
+                }}
+              >
+                Logout
+              </button>
+
+            ) : (
+
+              <button
+                style={styles.loginBtn}
+                onClick={() =>
+                  navigate("/login")
+                }
+              >
+                Login
+              </button>
+
+            )
+          }
+
+            {/* 🔹 Logout */}
+            <button
+              style={styles.logoutBtn}
+              onClick={logout}
+            >
+              Logout
+            </button>
+        
+              {/* 🔹 Cart Button */}
+              <button
+                style={styles.cartBtn}
+                onClick={() => navigate("/cart")}
+              >
+                Cart ({cart.length})
+              </button>
 
       </div>
     </div>
@@ -79,7 +114,26 @@ const styles = {
     cursor: "pointer",
     background: "crimson",
     color: "white"
-  }
+  },
+  loginBtn: {
+  padding: "10px 18px",
+  border: "none",
+  background: "#6c63ff",
+  color: "#fff",
+  borderRadius: "8px",
+  cursor: "pointer",
+  marginLeft: "15px"
+},
+
+// logoutBtn: {
+//   padding: "10px 18px",
+//   border: "none",
+//   background: "crimson",
+//   color: "#fff",
+//   borderRadius: "8px",
+//   cursor: "pointer",
+//   marginLeft: "15px"
+// },
 };
 
 export default Navbar;
